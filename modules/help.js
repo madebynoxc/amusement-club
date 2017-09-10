d8c0a21b6dc946533c31ea2c73296d9ce2a37453
NoxCaos <glebx95@gmail.com>
2017-09-09 20:04:56 -0400

    Added advanced help

--- modules/help.js
+++ modules/help.js
@@ -1,20 +1,28 @@
 module.exports = {
-
+    processRequest
 }
 
 const Discord = require("discord.js");
 const logger = require('./log.js');
 const utils = require('./localutils.js');
 const helpBody = require('../help/general.json');
+const helpAll = require('../help/modules.json');
+const settings = require('../settings/general.json');
 
 function processRequest(message, args, callback) {
     var req = args.shift();
     if(req) {
-    	
-    } else showGeneral(message);
+        var help = helpAll.filter(h => h.type.includes(req))[0];
+        if(help) {
+            message.author.send("__**" + help.title + "**__\n\n" + help.body);
+            if(message.channel.name) 
+                callback("**" + message.author.username + "**, I've sent you a DM");
+        } 
+        else callback("Can't find help inforamtion for " + req);
+    } else showGeneral(message, callback);
 }
 
-function showGeneral(message) {
+function showGeneral(message, callback) {
 	let e = new Discord.RichEmbed();
     e.setColor(settings.botcolor);
     e.setAuthor(helpBody.title)
@@ -23,6 +31,6 @@ function showGeneral(message) {
     }, this);
 
     message.author.send("", { embed: e });
-
-    if(message.channel.name) return "**" + message.author.username + "**, I've sent you a DM";
+    if(message.channel.name) 
+        callback("**" + message.author.username + "**, I've sent you a DM");
 }
