8e4106e21541dc8fa8af793fbe11a1cdd989a8bf
NoxCaos <glebx95@gmail.com>
2018-12-24 23:34:07 -0500

    Christmas event, adming functions in progress

--- modules/admin.js
+++ modules/admin.js
@@ -2,13 +2,14 @@ module.exports = {
     processRequest, connect
 }
 
-var mongodb, bot;
+var mongodb, bot, ucollection;
 const utils = require('./localutils');
 const settings = require('../settings/general.json');
 
 function connect(db, client) {
     mongodb = db;
     bot = client;
+    ucollection = db.collection("users");
 }
 
 function processRequest(user, channelID, args, callback) {
@@ -16,6 +17,12 @@ function processRequest(user, channelID, args, callback) {
     switch(command) {
         case 'embed':
             embed(args, callback);
+            break;
+        case 'warn':
+            warnUser(user, args, callback);
+            break;
+        case 'ban':
+            banUser(args, callback);
             break;
     }
 }
@@ -25,4 +32,18 @@ function embed(args, callback) {
     console.log(utils.formatImage(null, null, "Formatted sample", link));
     
     callback(utils.formatImage(null, null, "Formatted sample", link));
+}
+
+async function warnUser(user, args, callback) {
+    let parse = utils.getUserID(args);
+    if(!parse.id)
+        return callback(utils.formatError(user, null, "please provide user ID"));
+
+    let res = await ucollection.update({ "discord_id": parse.id}, { $inc: { warnings: 1 }});
+    
+    if(res.result.nModified > 0) {
+        callback(utils.formatConfirm(user, null, `user was warned`));
+    } else 
+        callback(utils.formatError(user, null, `user with that ID was not found`));
+
 }
